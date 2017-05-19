8b257518a2306359ff5c499fb92b705695d1ef0b
Wojtek <wzygadlo@users.noreply.github.com>
2017-05-19 13:20:56 +0100

    reformating

--- dummy_resume.py
+++ dummy_resume.py
@@ -24,30 +24,30 @@ class Resume(self):
 
     def skills(self):
         skill = input()
-        if skill == "Linunx":
+        if skill == "Linux":
             print("RHEL5/6/7, Debian, Ubuntu, SLES 11/12"
-                "FreeBSD"
-                "Bonding"
-                "IPtables"
-                "RAID, LVM, LUKS Disk Encryption")
+                '''FreeBSD
+                Bonding
+                IPtables
+                RAID, LVM, LUKS Disk Encryption")'''
 
         elif skill == "Network Skills":
-            print("Switching (VLAN, Trunking, Link Aggregation), Wireless"
-                "Routing (RIPv2, OSPF, EIGRP)"
-                "Load balancing (HAProxy)"
-                "FibreChannel"
-                "TCP/IP"
-                "Network monitoring and analysing (Wireshark, nmap,Nagios)"
-                "Server setup (Apache, nginx, SSH, FTP, DHCP, NTP, SMB, NFS, OpenLDAP, VPN, AFP, SSL)")
+            print('''Switching (VLAN, Trunking, Link Aggregation), Wireless
+                Routing (RIPv2, OSPF, EIGRP),
+                Load balancing (HAProxy),
+                FibreChannel,
+                TCP/IP,
+                Network monitoring and analysing (Wireshark, nmap,Nagios),
+                Server setup (Apache, nginx, SSH, FTP, DHCP, NTP, SMB, NFS, OpenLDAP, VPN, AFP, SSL)'''
 
         elif skill == "Programming":
             print("Shell/BASH" "Python")
 
         elif skill == "Database":
-            print("Oracle 10/11g DBA"
-                "MySQL DBA"
-                "MongoDB (query level)"
-                "SQL/SQLPlus")
+            print('''Oracle 10/11g DBA"
+                MySQL DBA,
+                MongoDB (query level),
+                SQL/SQLPlus)'''
 
         elif skill == "Cloud Solutions":
             print("Openstack"
@@ -64,75 +64,75 @@ class Resume(self):
         if start_time == "05/2013" and end_time == "Current":
             work_history = {"Cisco Systems LTD": "Systems Engineer",
                             "Location": "Southampton, UK",
-                            "Duties": [
-                                "- Managing and monitoring all installed systems and infrastructure 
-                                   to ensure the highest level of availability. (Nagios/ELK/Grafana)",
-                                "- Installing, configuring and testing operating systems, application software",
-                                "- Managing security, upgrade and deployment projects on-site.",
-                                "- Built and provided high-level E2E, troubleshooting on Windows, Linux Systems"
-                                "- Overseeing development and maintenance of customers computer systems
-                                   in Broadcasting industry"
-                                "- Maintain 24/7 availability for responsible systems. (On-call rotation)"
-                                "- Providing CI based on Jenkins solution."
-                                "- Monitoring and testing application performance to identify potential bottlenecks, 
-                                   developing solutions, and collaborate with developers 
-                                   on solution implementation."
-                                "- Writing and maintaining custom scripts to increase system efficiency 
-                                   and performance time."
-                                "- Providing 2nd and 3rd level technical support and troubleshooting 
-                                   to internal and external clients."
-                            ]}
+                            "Duties": ['''
+                                - Managing and monitoring all installed systems and infrastructure 
+                                   to ensure the highest level of availability. (Nagios/ELK/Grafana).
+                                - Installing, configuring and testing operating systems, application software.
+                                - Managing security, upgrade and deployment projects on-site.
+                                - Built and provided high-level E2E, troubleshooting on Windows, Linux Systems.
+                                - Overseeing development and maintenance of customers computer systems
+                                   in Broadcasting industry,
+                                - Maintain 24/7 availability for responsible systems. (On-call rotation).
+                                - Providing CI based on Jenkins solution.
+                                - Monitoring and testing application performance to identify potential bottlenecks, 
+                                   developing solutions, and collaborate with developers
+                                   on solution implementation.
+                                - Writing and maintaining custom scripts to increase system efficiency 
+                                   and performance time.
+                                - Providing 2nd and 3rd level technical support and troubleshooting 
+                                   to internal and external clients.
+                            ''']}
                             print(work_history)
 
             elif start_time == "02/2011" and end_time == "05/2013":
                 work_history = {"Nokia Siemens Network": "Technical Support Engineer",
                                 "Location": "Wroclaw, Poland",
-                                "Duties": [
-                                    "- Testing (E2E, IOT)",
-                                    "- Trailing activities",
-                                    "- Providing 24/7 support (on-call rotation)",
-                                    "- Design and implementation of test environment 
-                                       based on Linux RedHat servers.",
-                                    "- Implementing VLAN solution at the customer side 
-                                       for Femto/Small Cells network.",
-                                    "- Features verification, testing new features in new SW release.",
-                                    "- Remote and onsite support for customers all over the world",
-                                    "- Providing internal training for customer teams regarding NSN Femto Solution",
-                                    "- Managing and maintenance experience in Core Networks (MSC, SGSN) 
-                                       during Femto integration at customer sites",
-                                    "- Tracking problems and solutions based on Resolve BMC Remedy, 
+                                "Duties": ['''
+                                    - Testing (E2E, IOT),
+                                    - Trailing activities,
+                                    - Providing 24/7 support (on-call rotation),
+                                    - Design and implementation of test environment 
+                                       based on Linux RedHat servers.,
+                                    - Implementing VLAN solution at the customer side 
+                                       for Femto/Small Cells network.,
+                                    - Features verification, testing new features in new SW release.,
+                                    - Remote and onsite support for customers all over the world,
+                                    - Providing internal training for customer teams regarding NSN Femto Solution,
+                                    - Managing and maintenance experience in Core Networks (MSC, SGSN) 
+                                       during Femto integration at customer sites,
+                                    - Tracking problems and solutions based on Resolve BMC Remedy, 
                                        JIRA, Service Desk."
-                                ]}
+                                ''']}
                                 print(work_history)
 
             elif start_time == "10/2007" and end_time == "02/2011":
                 work_history = {"TECE": "Network Administrator",
                                 "Location": "Strzelin, Poland",
-                                "Duties": [
-                                    "- Installation, upgrading and maintaining Servers Windows Server 2003 R2",
-                                    "- Managing Web server (Apache, IIS)",
-                                    "- Linux server administration based on Debian 
+                                "Duties": ['''
+                                    - Installation, upgrading and maintaining Servers Windows Server 2003 R2,
+                                    - Managing Web server (Apache, IIS),
+                                    - Linux server administration based on Debian 
                                        (DHCP, Proxy server, Samba, IPtables) 
                                        Novell SLES (Backup email server based on eDirecotry) 
-                                       Ubuntu with KVM for virtualisation.",
-                                    "- Cisco device management (Switches, Routers)",
-                                    "- Configuration routeing table",
-                                    "- Maintaining VPN connection between TECE headquarter and branch offices",
-                                    "- Creating websites (PHP, CSS, HTML)",
-                                    "- Database administration of MySQL, MSSQL",
-                                    "- Helpdesk for 60 peoples in the TECE Poland office."
-                                ]}
+                                       Ubuntu with KVM for virtualisation.,
+                                    - Cisco device management (Switches, Routers),
+                                    - Configuration routeing table,
+                                    - Maintaining VPN connection between TECE headquarter and branch offices,
+                                    - Creating websites (PHP, CSS, HTML),
+                                    - Database administration of MySQL, MSSQL,
+                                    - Helpdesk for 60 peoples in the TECE Poland office.
+                                ''']}
                                 print(work_history)
 
             elif start_time == "11/2005" and end_time == "10/2007":
                 work_history = {"PHOUA SP z o.o.": "Network Administrator",
                                 "Locaton": "Wroclaw, Poland",
-                                "Duties": [
-                                    "- Managing network infrastructure"
-                                    "- Implementation WLAN access on Cisco devices"
-                                    "- Maintaining Windows systems"
-                                    "- Helpdesk"
-                                ]}
+                                "Duties": ['''
+                                    - Managing network infrastructure
+                                    - Implementation WLAN access on Cisco devices
+                                    - Maintaining Windows systems
+                                    - Helpdesk
+                                ''']}
                                 print(work_history)
 
             elif start_time == "01/2005" and end_time "09/2005":
